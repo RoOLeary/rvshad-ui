716f168746023220901127eb9939d3c4db85440b
Ronan O'Leary <ronanoleary@Ronans-MacBook-Pro-2.local>
2024-11-11 12:46:06 +0100

    additional shad assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import CountBtn from "@/components/count-btn";
 import { Badge } from "@/components/ui/badge";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
 import SearchProgress from "@/components/search-progress";
 import { ContentCarousel } from "@/components/content-carousel";
 
@@ -24,6 +31,28 @@ function App() {
           <Badge variant="outline">Findest Design System/ui</Badge>
         </a>
         <CountBtn />
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>Is it styled?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>Is it animated?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It&apos;s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>  
         <ContentCarousel />
         <SearchProgress />
       </div>
